b65afca510fb0b3ee666079b87b4c41499d787d5
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2024-01-15 01:55:59 +0900

    chore: change build script.

--- bin/build.ts
+++ bin/build.ts
@@ -1,23 +1,63 @@
 import {promises} from 'fs';
 import { basename, resolve } from 'path';
+import * as esbuild from 'esbuild';
 
 (async () => {
-const files = await promises.readdir(resolve('./src/language/'));
+const files = await promises.readdir(resolve('./src/ruleMessages/'));
 
-const languageFiles = files.map(async file => {
-  const {translation} = await import(`../src/language/${basename(file, '.ts')}`);
-  const fileContent = `import {generateRules} from './translation';
-import {translation} from './language/${basename(file, '.ts')}';
-  
-const rules = generateRules(translation);
-
-${[...Object.keys(translation)]
-  .map(ruleName => `export const ${ruleName} = rules.${ruleName};`)
-  .join('\n')}
-`;
-
-  return promises.writeFile(resolve(`./src/${file}`), fileContent);
-});
-
-await Promise.all(languageFiles);
+await generateLanguageFiles(files);
+await generateBundledFiles(files);
 })();
+
+
+async function generateLanguageFiles(files: string[]) {
+  const languageFiles = files.map(async file => {
+    const {messages} = await import(`../src/ruleMessages/${basename(file, '.ts')}`);
+    const fileContent = `import {generateRules} from '../translation';
+  import {messages} from '../ruleMessages/${basename(file, '.ts')}';
+  export * from 'class-validator';
+    
+  const rules = generateRules(messages);
+  
+  ${[...Object.keys(messages)]
+    .map(ruleName => `export const ${ruleName} = rules.${ruleName};`)
+    .join('\n')}
+  `;
+  
+    return promises.writeFile(resolve(`./src/languages/${file}`), fileContent);
+  });
+  
+  await Promise.all(languageFiles);
+}
+
+async function generateBundledFiles(files: string[]) {
+  return Promise.all(files.flatMap(file => [
+    esbuild.build({
+      entryPoints: [resolve(`./src/languages/${file}`)],
+      bundle: true,
+      //minify: true,
+      sourcemap: true,
+      format: 'esm',
+      packages: 'external',
+      outfile: `dist/${basename(file, '.ts')}.esm.js`,
+    }),
+    esbuild.build({
+      entryPoints: [resolve(`./src/languages/${file}`)],
+      bundle: true,
+      //minify: true,
+      sourcemap: true,
+      format: 'esm',
+      packages: 'external',
+      outfile: `dist/${basename(file, '.ts')}.mjs`,
+    }),
+    esbuild.build({
+      entryPoints: [resolve(`./src/languages/${file}`)],
+      bundle: true,
+      platform: 'node',
+      packages: 'external',
+      //minify: true,
+      sourcemap: true,
+      outfile: `dist/${basename(file, '.ts')}.cjs.js`,
+    }),
+  ]));
+}
